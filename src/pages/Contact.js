b650197cbcf48f66da54bb1a3b651380dd85a9d8
Wassim Abdennadher <wassim.abd@gmail.com>
2023-01-15 16:01:35 +0100

    add the contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
 function Contact() {
+  const contactInfo = {
+    mail: 'Wassim.abd@gmail.com',
+    phoneNumber: '+216 58 48 48 11',
+    adress: 'Route mharza km 3 cité el habib sfax, Tunisia'
+  }
   return (
-    <div>Contact</div>
+    <div className='alert alert-secondary mt-5 fs-5 fw-bold '>
+      <div>
+        <i className="bi bi-envelope-at"></i>
+        <p>: {contactInfo.mail}</p>
+      </div>
+      <div>
+        <i className="bi bi-telephone-x-fill"></i>
+        <p>: {contactInfo.phoneNumber}</p>
+      </div>
+      <div>
+        <i className="bi bi-geo-alt-fill">
+        </i><p>: {contactInfo.adress}</p>
+      </div>
+    </div>
   )
 }
 
