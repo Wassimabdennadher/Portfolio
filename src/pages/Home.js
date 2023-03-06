83db9c231801f7a6bddbb0f95c00f4e80504f102
Wassim Abdennadher <wassim.abd@gmail.com>
2023-03-06 18:35:10 +0100

    Chnage image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import devImg from '../images/4884785.jpg'
+import devImg from '../images/mern3.jpg'
 
 function Home() {
   return (
     <div className="alert alert-dark mt-5" role="alert">
     <img src="./profile.png" alt="profile" style={{width:200, borderRadius:'50%'}}/> 
     <p className='mt-2' style={{fontSize:40}}><strong>Wassim abdennadher</strong></p> 
-    <p>Hello, my name is <strong>Wassim Abdennadher</strong> and i'am a <strong>FullStack JS (React)</strong> Web developer</p>
+    <p>Hello, my name is <strong>Wassim Abdennadher</strong> and i'am a <strong>FullStack JS (MERN)</strong> Web developer</p>
     <img src={devImg} alt="Dev" style={{width:'100%', borderRadius:'5px'}}/>
     </div>
   )
