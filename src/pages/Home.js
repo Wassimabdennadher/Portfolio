03bcb1e29e51f6ff422b9af6ed433cd631235304
Wassim Abdennadher <wassim.abd@gmail.com>
2023-04-10 16:18:05 +0100

    edit the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,9 @@ import devImg from '../images/mern3.jpg'
 function Home() {
   return (
     <div className="alert alert-dark mt-5" role="alert">
-    <img src="./profile.png" alt="profile" style={{width:200, borderRadius:'50%'}}/> 
+    <img src="./profile.png" alt="profile" style={{width:200, borderRadius:'20px' }}/> 
     <p className='mt-2' style={{fontSize:40}}><strong>Wassim abdennadher</strong></p> 
-    <p>Hello, my name is <strong>Wassim Abdennadher</strong> and i'am a <strong>FullStack JS (MERN)</strong> Web developer</p>
+    <p style={{fontSize:'25px'}}>Hello, my name is <strong>Wassim Abdennadher</strong> and i'am a <strong>FullStack JS (MERN)</strong> Web developer</p>
     <img src={devImg} alt="Dev" style={{width:'100%', borderRadius:'5px'}}/>
     </div>
   )
